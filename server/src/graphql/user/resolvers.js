0f95e4a655c0169c1bddae40ff59e84480cafa50
Praveen Bisht <prvnbist@gmail.com>
2020-02-26 22:52:51 +0530

    feat(resolvers): updateUser

--- server/src/graphql/user/resolvers.js
+++ server/src/graphql/user/resolvers.js
@@ -30,6 +30,44 @@ module.exports = {
             throw new UserInputError(error.message)
          }
       },
+      updateUser: async (_, { input }) => {
+         try {
+            const { id, ...fields } = input
+
+            // Check if new username is same as existing
+            if (fields.username) {
+               const { username } = await User.findById(id)
+               if (fields.username === username) {
+                  throw new Error("New username can't same as existing!")
+               }
+            }
+
+            // Check if username is already taken
+            if (fields.username) {
+               const userExists = await User.findOne({
+                  username: fields.username
+               })
+               if (userExists) {
+                  throw new Error('Username already taken')
+               }
+            }
+
+            // Update user details
+            const user = await User.findByIdAndUpdate(
+               id,
+               { ...fields },
+               { new: true }
+            )
+            return {
+               code: '200',
+               success: true,
+               message: 'User updated successfully!',
+               user
+            }
+         } catch (error) {
+            throw new UserInputError(error.message)
+         }
+      },
       deleteUser: async (_, { id }) => {
          try {
             await User.findByIdAndUpdate(id, { isActive: false })
