59c00d390a65aeaf85b1d1b687fa8f4bf873c108
Praveen Bisht <prvnbist@gmail.com>
2020-02-28 11:01:26 +0530

    refactor(query): removed redundant queries

--- server/src/graphql/queries/index.js
+++ server/src/graphql/queries/index.js
@@ -2,16 +2,8 @@ const { gql } = require('apollo-server-express')
 
 const typeDef = gql`
    type Query {
-      "List of users"
-      users: [User]!
       "User's profile"
       user(id: ID!): User!
-      "Get followers of a user"
-      followers(id: ID!): [User]!
-      "Get user's followings"
-      following(id: ID!): [User]!
-      "Get user's liked tweets"
-      liked(id: ID!): [Woof]!
       "List of global woofs or by a user"
       woofs(id: ID): [Woof]!
       "Get a woof by woof ID"
