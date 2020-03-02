38bdefdfd2b1680871cf40b1dd8b51a6d53ffb47
Praveen Bisht <prvnbist@gmail.com>
2020-03-03 00:08:39 +0530

    feat(client/header): added logo

--- client/sections/Header/index.jsx
+++ client/sections/Header/index.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { StyledHeader, StyledLogo } from './styled'
+
 const Header = () => {
-   return <header>Header</header>
+   return (
+      <StyledHeader>
+         <StyledLogo>
+            <img src="/static/svgs/logo.svg" alt="Woofer" />
+         </StyledLogo>
+      </StyledHeader>
+   )
 }
 
 export default Header
---
+++ client/sections/Header/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.header`
+   height: 48px;
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   border-bottom: 1px solid #e9e9e9;
+`
+
+export const StyledLogo = styled.div`
+   width: 48px;
+   display: flex;
+   height: inherit;
+   align-items: center;
+   justify-content: center;
+`
