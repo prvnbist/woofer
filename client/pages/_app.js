acf469fcba7fb3c583da63765f93bb38028e85f8
Praveen Bisht <prvnbist@gmail.com>
2020-03-02 23:58:19 +0530

    feat(client): added styled-components

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,30 @@
-import '../global.css'
+import React from 'react'
 
-export default function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />
+import App from 'next/app'
+import Head from 'next/head'
+
+import '../global.css'
+export default class MyApp extends App {
+   static async getInitialProps({ Component, ctx }) {
+      let pageProps = {}
+
+      if (Component.getInitialProps) {
+         pageProps = await Component.getInitialProps(ctx)
+      }
+
+      return { pageProps }
+   }
+
+   render() {
+      const { Component, pageProps } = this.props
+
+      return (
+         <>
+            <Head>
+               <title>Heap</title>
+            </Head>
+            <Component {...pageProps} />
+         </>
+      )
+   }
 }
