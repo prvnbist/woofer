90e2ee4db227e737138dc919263a229fef684fca
Praveen Bisht <prvnbist@gmail.com>
2020-03-02 23:16:00 +0530

    feat(client): nextjs setup

---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Home = () => (
+   <div>
+      <Head>
+         <title>Home - Woofer</title>
+      </Head>
+      HomePage
+   </div>
+)
+
+export default Home
